79984fdd1fdb168eb2cb0b3ef38ebdf30734500e
Moqleh <moh.oglah@gmail.com>
2026-09-19 09:46:12 +0300

    Complete Tabkhat Al Yawm PWA

---
+++ tabkhat-al-yawm/sw.js
@@ -0,0 +1,1 @@
+const C="tabkhat-v1";self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./index.html","../assets/img/cooking.jpg"]))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
